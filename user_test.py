48610c807962ea38f8fe7cf54d7aef8823e9fded
Kiptoo Rugut <mccrug97@gmail.com>
2022-02-20 23:33:30 +0300

    it will create user_test file methods

---
+++ user_test.py
@@ -0,0 +1,26 @@
+import unittest
+from user import User
+
+
+class TestUser(unittest.TestCase):
+    '''
+     Test class that defines test cases for the user class behaviours.
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    
+    def setUp(self):
+        '''
+        Set up function for the test class to create a new user
+        '''
+        self.new_user = User("kchamdany", "45chamdany")
+
+    def test_user_created(self):
+        '''
+        Function to test to find if the user object is created successfully
+        '''
+        self.assertEqual(self.new_user.username, "kchamdany")
+        self.assertEqual(self.new_user.password, "45chamdany")
+
+
+    
