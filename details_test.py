365b2f4a4ebe767efd94b8b4ece8957c8410cdd3
Kiptoo Rugut <mccrug97@gmail.com>
2022-02-20 18:20:47 +0300

    it will add  python test framework, unittest. And also import the Details class.

---
+++ details_test.py
@@ -0,0 +1,10 @@
+import unittest  #Importing unittest module
+from details import Details  #Importing the details class 
+
+
+# class TestDetails(unittest.TestCase):
+#     '''
+#     Test class that defines test cases for the details class behaviours.
+#     Args:
+#          unittest.TestCase: TestCase class that helps in creating test cases
+#     '''
