2e9c8d6a053121f4963479a21ea453c37671682b
Kiptoo Rugut <mccrug97@gmail.com>
2022-02-20 23:00:23 +0300

    it will add tests function for saving, deleting,finding and displaying details

--- details_test.py
+++ details_test.py
@@ -13,7 +13,7 @@ class TestDetails(unittest.TestCase):
         '''
         Set up method to run testing each test cases
         '''
-        self.new_details = Details("Instagrtam", "Miguel", "Limits454")
+        self.new_details = Details("Instagram", "Miguel", "Limits454")
         
     def test_init(self):
         '''
@@ -28,3 +28,72 @@ class TestDetails(unittest.TestCase):
             tearDown method cleans up after each test case has run.
         '''
         Details.user_details = []
+        
+
+    def test_save_details(self):
+
+        '''
+        test_save_details test case, test to ascertain if the details object is saved into the user_details
+        '''
+        self.assertEqual(len(Details.user_details), 0)
+        self.new_details.save_details()
+        self.assertEqual(len(Details.user_details),1)
+
+
+    def test_save_multiple_details(self):
+            '''
+            test_save_multiple_details to check if we can save multiple credential
+            objects to our user_details
+            '''
+            self.new_details.save_details()
+            test_details = Details("Test","user_name","Limits454") 
+            test_details.save_details()
+            self.assertEqual(len(Details.user_details),2)
+
+
+    def test_delete_details(self):
+            '''
+            test if we can remove details from our user_details list
+            '''
+            self.assertEqual(len(Details.user_details), 0)
+            self.new_details.save_details()
+            self.assertEqual(len(Details.user_details), 1)
+            self.new_details.delete_details()
+            self.assertEqual(len(Details.user_details), 0)
+
+
+    def test_find_details_by_account_socialmedia(self):
+        '''
+        test to check if we can find details by accout_socialmedia and display
+        '''
+
+        self.found_details = Details.find_by_account_socialmedia("Instagram")
+
+    def test_details_exists(self):
+        '''
+        test to check if we can return a Boolean  if we cannot find user details.
+        '''
+
+        self.new_details.save_details()
+        test_details = Details("Test","user_name","Limits454")
+        test_details.save_details()
+
+        details_exists = Details.details_exist("Test")
+
+        self.assertTrue(details_exists)
+
+
+    def test_display_all_details(self):
+        '''
+        method that returns a list of all details saved
+        '''
+
+        self.assertEqual(Details.display_details(),Details.user_details)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+    
