fb3980bd0fd6322c3384c3a5a238fce09882e366
Kiptoo Rugut <mccrug97@gmail.com>
2022-02-20 12:47:45 +0300

    it will add __init__ method that will create new intances of a class

---
+++ user.py
@@ -0,0 +1,16 @@
+class User:
+    '''
+    Class that generates new intances of Users
+    '''
+    user_list = []
+
+    def __init__(self, username, password):
+        '''
+        Function that initializes the user
+        '''
+        self.username = username
+        self.password = password
+
+   
+
+    
